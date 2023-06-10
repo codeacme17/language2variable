95871481d2fed0f95f30c16833da6a5df1c1de21
codeacme17 <vir_kings@qq.com>
2023-06-10 15:16:35 +0800

    feat: add `translator` util

--- src/extension.ts
+++ src/extension.ts
@@ -11,15 +11,42 @@ export function activate(context: vscode.ExtensionContext) {
 
 async function userInput() {
   const input = await vscode.window.showInputBox({
-    prompt: 'Enter text to insert',
+    prompt: '输入您的中文变量名',
   })
 
   if (input) {
-    const editor = vscode.window.activeTextEditor
-    if (editor) {
-      editor.edit((editBuilder) => {
-        editBuilder.insert(editor.selection.active, input)
-      })
-    }
+    const translations = await myTranslator(input)
+
+    const quickPick = vscode.window.createQuickPick()
+
+    quickPick.items = translations.map((translation) => ({
+      label: translation,
+    }))
+
+    quickPick.show()
+
+    quickPick.onDidChangeSelection((selection) => {
+      if (selection && selection[0]) {
+        const selectedTranslation = selection[0].label
+        const editor = vscode.window.activeTextEditor
+        if (editor) {
+          editor.edit((editBuilder) => {
+            editBuilder.insert(editor.selection.active, selectedTranslation)
+          })
+
+          quickPick.hide()
+        }
+      }
+    })
   }
+}
+
+function myTranslator(text: string): Promise<string[]> {
+  const translations = [
+    'Translation 1',
+    'Translation 2',
+    'Translation 3',
+    'Translation 4',
+  ]
+  return Promise.resolve(translations)
 }
